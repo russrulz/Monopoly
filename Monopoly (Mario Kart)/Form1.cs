e146b73dbc160611368f7b0d0692f6cc5ccc1f91
russrulz <58643273+russrulz@users.noreply.github.com>
2021-04-07 14:23:12 +1200

    Initial commit

---
+++ Monopoly (Mario Kart)/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Monopoly__Mario_Kart_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int[] Dice = new int[] { 1, 2, 3, 4, 5, 6 };
+        string[] Powerdice = new string[] { "coin","bannana","greenshell","lightning","blueshell","coin" };
+        struct race { };
+        struct property { };
+        struct character { };
+        List<race> Races; //add all race cards to set
+        List<property>
+
+    }
+}
