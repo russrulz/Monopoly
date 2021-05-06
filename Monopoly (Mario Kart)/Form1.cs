4da712912bf502e318207153590ce9bdcc92e4ac
russrulz <58643273+russrulz@users.noreply.github.com>
2021-05-06 15:35:50 +1200

    Add Racers
    Add racers info to list of racers

--- Monopoly (Mario Kart)/Form1.cs
+++ Monopoly (Mario Kart)/Form1.cs
@@ -18,15 +18,25 @@ namespace Monopoly__Mario_Kart_
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            characters.Add(new Racer("Bowser","Steal 4 Coins","Blue Shell: Player with most coins drops 3 all others drop 1"));
+            characters.Add(new Racer("Metal Mario", "All other players skip next powerup die roll", "Lightning: All other players drop 1 coin, Roll number die and move again"));
+            characters.Add(new Racer("Shy Guy", "Move up to 3 spaces forward or backward follow rules of space you land on", "Green Shell: the players in front and behind drop 3 coins"));
+            characters.Add(new Racer("Yoshi", "Collect all coins on the board", "Green Shell: The player in front drops 3 coins you may collect them without moving"));
+            characters.Add(new Racer("Donkey Kong", "You may place 1 bannana on up to 2 properties you own", "Bannana: You may remove any number of bananas collect 3 per banana removed"));
+            characters.Add(new Racer("Rosalina", "You may move to the next superstar sapce collecting any coins you pass", "Coin: take 1 coin from each player"));
+            characters.Add(new Racer("Luigi", "You may move the the least expensive unowned property and buy or auction it", "Bannana: place a banana on one of your properties"));
+            characters.Add(new Racer("Toad", "You may drop up to 5 coins to move that many spaces", "Lightning: All places drop 1 coin and give you 1 coin"));
+            characters.Add(new Racer("Princess Peach","At the end of your turn roll powerup die and coplete the action", "Blue Shell: Choose 2 players to drop 3 coins"));
+            characters.Add(new Racer("Mario", "Collect 3 coins then roll number die and move again","Collect 3 Coins or add up to 3 to your roll"));
         }
         int[] Dice = new int[] { 1, 2, 3, 4, 5, 6 };
-        string[] Powerdice = new string[] { "coin","bannana","greenshell","lightning","blueshell","coin" };
-        struct race { };
-        struct property { };
-        struct character { };
+        string[] Powerdice = new string[] { "Coin", "Bannana", "Green Shell", "Lightning", "Blue Shell", "Coin" };
+        public struct race { };
+        public struct property { };
         List<race> Races; //add all race cards to set
-        List<property>
+        List<property> Properties;// add properties to list 
+        List<Racer> characters;
 
+    
     }
 }
